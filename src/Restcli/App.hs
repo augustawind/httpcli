57fab72bd5f8e0153906c7598e3602899c9c3ce4
Dustin Rohde <dustin.rohde@gmail.com>
2020-06-29 12:13:58 -0400

    successfully execute scripts (no persistence tho)

--- src/Restcli/App.hs
+++ src/Restcli/App.hs
@@ -1,15 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Restcli.App where
 
 import           Control.Exception
 import           Control.Monad.Reader
 import           Control.Monad.State
+import           Data.Aeson              hiding ( Options )
 import           Data.ByteString.Char8          ( ByteString )
 import qualified Data.ByteString.Char8         as B
-import qualified Data.HashMap.Strict.InsOrd    as Map
+import qualified Data.ByteString.Lazy.Char8    as LB
+import qualified Data.CaseInsensitive          as CI
+import           Data.HashMap.Strict            ( HashMap )
+import qualified Data.HashMap.Strict           as Map
+import           Data.HashMap.Strict.InsOrd     ( InsOrdHashMap )
+import qualified Data.HashMap.Strict.InsOrd    as OrdMap
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 import qualified Data.Text.Lazy                as LT
+import           Data.Text.Encoding             ( decodeUtf8
+                                                , encodeUtf8
+                                                )
 import qualified Data.Yaml                     as Yaml
+import qualified Foreign.Lua                   as Lua
+import           Foreign.Lua.Aeson
 import           Text.Mustache                  ( Template )
 import qualified Text.Pretty.Simple            as PP
 
@@ -50,7 +64,7 @@ runApp app = do
     tmpl <- readApiTemplate $ optApiFile opts
     env  <- case optEnvFile opts of
         Just filePath -> readEnv filePath
-        Nothing       -> return $ Env Map.empty
+        Nothing       -> return $ Env OrdMap.empty
 
     case parseAPI tmpl env of
         Left err -> fail $ displayException err
@@ -91,7 +105,49 @@ cmdRun path = do
     let (groupKeys, reqKey) = unsnoc path
     case getApiRequest groupKeys reqKey api of
         Left  err -> fail $ displayException err
-        Right req -> liftIO $ B.pack . pshow <$> sendRequest req
+        Right req -> do
+            res <- liftIO $ sendRequest req
+            case reqScript req of
+                Nothing     -> return ()
+                Just script -> execScript script res
+            return . B.pack . pshow $ res
+
+execScript :: Text -> HttpResponse -> App ()
+execScript script res = do
+    env <- gets appEnv
+    let ctx = mkScriptContext res env
+    liftIO . Lua.run $ do
+        Lua.openlibs
+        Lua.push (mkScriptContext res env) *> Lua.setglobal' "ctx"
+        result <- Lua.dostring (encodeUtf8 script)
+        case result of
+            Lua.OK -> return ()
+            err    -> Lua.peek 1 >>= liftIO . fail
+
+mkScriptContext :: HttpResponse -> Env -> HashMap String Value
+mkScriptContext HttpResponse {..} (Env env) = Map.fromList
+    [ ( "response"
+      , object
+          [ "version" .= (String . T.pack . show $ resHttpVersion)
+          , "status_code" .= (Number . fromIntegral $ resStatusCode)
+          , "status" .= String
+              (T.unwords
+                  [ T.pack . show $ resStatusCode
+                  , decodeUtf8 . LB.toStrict $ resStatusText
+                  ]
+              )
+          , "headers"
+              .= toJSON
+                     (map
+                         (\(k, v) -> (B.unpack $ CI.foldedCase k, B.unpack v))
+                         resHeaders
+                     )
+          -- TODO: add error handling (change func sig to App, or maybe just Either)
+          , "body" .= (either error id (eitherDecode' resBody) :: Value)
+          ]
+      )
+    , ("env", Object $ OrdMap.toHashMap env)
+    ]
 
 -- | Execute the `view` command.
 cmdView :: [Text] -> App ByteString
