cbc45a20c446bbfcef17e6df7892b51660aae3e5
Dustin Rohde <dustin.rohde@gmail.com>
2020-07-16 10:47:18 -0400

    organize examples

---
+++ examples/httpbin/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# setup.sh
+# Source this file from the package root to configure httpcli for this example.
+#
+export HTTPCLI_API="./examples/httpbin/api.yaml"
+export HTTPCLI_ENV="./examples/httpbin/env.yaml"
---
+++ examples/simple/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# setup.sh
+# Source this file from the package root to configure httpcli for this example.
+#
+export HTTPCLI_API="./examples/simple/api.yaml"
+export HTTPCLI_ENV="./examples/simple/env.yaml"
