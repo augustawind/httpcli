adf14a87316803548198835e15120a6087b0c3d2
Dustin Rohde <dustin.rohde@gmail.com>
2020-07-17 01:25:35 -0400

    reqmods parsing

--- src/Restcli/CLI.hs
+++ src/Restcli/CLI.hs
@@ -1,25 +1,42 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 
 module Restcli.CLI where
 
 import           Control.Applicative            ( optional )
+import           Control.Exception              ( displayException )
+import           Data.Aeson              hiding ( Options )
+import           Data.Bifunctor                 ( first )
+import           Data.ByteString                ( ByteString )
+import           Data.HashMap.Strict            ( HashMap )
+import qualified Data.HashMap.Strict           as Map
+import qualified Data.List                     as L
 import           Data.List.Split                ( splitOn )
+import           Data.Maybe
 import           Data.Semigroup                 ( (<>) )
 import           Data.String                    ( IsString
                                                 , fromString
                                                 )
+import           Data.Text                      ( Text )
+import qualified Data.Text                     as T
+import           Data.Text.Encoding             ( encodeUtf8 )
+import           Data.Yaml                      ( decodeEither' )
 import           Options.Applicative
 import           Options.Applicative.Types      ( readerAsk )
-import           System.Environment
 import           System.FilePath                ( joinPath )
+
+import           Restcli.Error
+import           Restcli.Types
 
 data Options = Options
     { optCommand :: Command
     , optAPIFile :: FilePath
     , optEnvFile :: Maybe FilePath
     , optSave :: Bool
+    , optReqMods :: [ReqMod]
     } deriving (Eq, Show)
 
 data Command
@@ -46,7 +63,7 @@ parseArgsPure argv environ = execParserPure parserPrefs
 parserInfo :: Environ -> ParserInfo Options
 parserInfo environ = info
   (parser environ <**> helper)
-  (fullDesc <> header
+  (briefDesc <> forwardOptions <> header
     (progName ++ " - a command-line HTTP client for API development")
   )
 
@@ -118,10 +135,59 @@ parser environ = do
     (long "save" <> short 's' <> help
       "save changes made to the Environment from request scripts"
     )
+  optReqMods <- many $ argument
+    reqModReader
+    (metavar "MODIFIER" <> help "modify request attributes on-the-fly")
+
   pure Options { .. }
  where
   mkCommand (name, desc, parser) =
     command name (info (parser <**> helper) (progDesc desc))
+
+data ReqMod
+    = ModHeader ByteString ByteString
+    | ModQuery Text (Maybe Text)
+    | ModStrField Text Text
+    | ModJsonField Text Value
+    deriving (Eq, Show)
+
+reqModReader :: ReadM ReqMod
+reqModReader = eitherReader $ \s -> case parse (T.pack s) of
+  Nothing     -> Left "invalid modifier"
+  Just result -> first displayException result
+ where
+  parse :: Text -> Maybe (Either Error ReqMod)
+  parse s = listToMaybe $ mapMaybe (`tryParse` s) delimiters
+  tryParse :: Text -> Text -> Maybe (Either Error ReqMod)
+  tryParse delim s =
+    let func  = fromJust $ lookup delim reqModParserMap
+        parts = T.splitOn delim s
+    in  if null parts || T.null (head parts)
+          then Nothing
+          else case parts of
+            [k, ""] -> Just $ func (head parts) Nothing
+            [k, v ] -> Just $ func (head parts) (Just $ parts !! 1)
+            _       -> Nothing
+  delimiters = map fst reqModParserMap
+
+type ReqModFunc = Text -> Maybe Text -> Either Error ReqMod
+
+reqModParserMap :: [(Text, ReqModFunc)]
+reqModParserMap =
+  [ (":=", parseJsonFieldMod)
+  , (":" , parseHeaderMod)
+  , ("==", parseQueryMod)
+  , ("=" , parseStrFieldMod)
+  ]
+ where
+  parseJsonFieldMod k v = case v of
+    Nothing   -> Right $ ModJsonField k Null
+    Just text -> either (Left . YamlError)
+                        (Right . ModJsonField k)
+                        (decodeEither' $ encodeUtf8 text)
+  parseHeaderMod k v = Right $ ModHeader (encodeUtf8 k) (maybe "" encodeUtf8 v)
+  parseQueryMod = fmap Right . ModQuery
+  parseStrFieldMod k v = Right $ ModStrField k (fromMaybe "" v)
 
 nonEmptyStr :: IsString s => ReadM s
 nonEmptyStr = eitherReader $ \case
